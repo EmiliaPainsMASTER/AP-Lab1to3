65b30b330860ac80f8087091cea3141347657e24
EmiliaPainsMASTER <B00165844@mytudublin.ie>
2025-02-13 14:53:52 +0000

    second

---
+++ Lab2/Lab2Part1/src/Lab2Part1/TryCatchTest.java
@@ -0,0 +1,16 @@
+package Lab2Part1;
+
+public class TryCatchTest {
+public void TryCatch(String name){
+    try{
+
+    } catch (StringIndexOutOfBoundsException e) {
+        throw new RuntimeException(e);
+    } finally {
+        // do something else
+    }
+}
+    public static void main(String[] args) {
+        //do something
+    }
+}
---
+++ Lab2/Lab2Part2/src/Lab2Part2/CheckedVersusUncheckedExceptions.java
@@ -0,0 +1,29 @@
+package Lab2Part2;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CheckedVersusUncheckedExceptions {
+    public static void capitalizeString(String s){
+        try{
+
+        }catch(NullPointerException e){
+            throw new NullPointerException();
+        }
+    }
+    public static void openFile(String fileName) throws IOException {
+        try{
+            File file = new File(fileName);
+            file.exists();
+        }catch(IOException e){
+            System.out.println("Cannot find file: " + e);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        String s = "null";
+        String fileName = "idea";
+//        capitalizeString(s);
+//        System.out.println(s);
+        openFile(fileName);
+    }
+}
